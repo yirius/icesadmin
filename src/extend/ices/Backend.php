35f4dc5c19893efeebbf732a05e72eb1b8b36f5e
yirius <2951979767@qq.com>
2018-08-02 17:14:13 +0800

    add bramub

--- src/extend/ices/Backend.php
+++ src/extend/ices/Backend.php
@@ -19,9 +19,11 @@ trait Backend
      * @description 再删除或者进行其他关键操作的时候,进行验证
      * @createtime: 2018/7/11 00:17
      * @param string $password 用户输入的密码 true '' ''
+     * @param int $access_type 获取到数据库的类型 false '' ''
+     * @param callable $encryptFunc 自己采用的加密方式 false '' ''
      * @return mixed 如果失败,直接返回json的输出,如果成功,返回用户信息
      */
-    protected function checkJwtPassword($password){
+    protected function checkJwtPassword($password, $access_type = 0, $encryptFunc = null){
         $access_token = input("param.access_token", '');
         if(empty($access_token)){
             Response::create([
@@ -31,8 +33,13 @@ trait Backend
             exit;
         }
         $userinfo = Adminjwt::instance()->decodeHmac($access_token);
-        $uinfo = Auth::instance()->getUserInfo($userinfo['id']);
-        if($uinfo['password'] != sha1($password . $uinfo['salt'])){
+        $uinfo = Auth::instance()->getUserInfo($userinfo['id'], $access_type);
+        //在这里，判断一下加密是否存在,如果存在方法，就使用方法去加密，如果不存在，就是用默认加密
+        $encryptData = is_null($encryptFunc) ?
+            sha1($password . $uinfo['salt']) :
+            $encryptFunc($password, $uinfo);
+        //判断加密
+        if($uinfo['password'] != $encryptData){
             Response::create([
                 'code' => 0,
                 'msg' => "您输入的密码不正确"
